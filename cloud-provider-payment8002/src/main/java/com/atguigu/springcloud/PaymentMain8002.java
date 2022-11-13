aced0859fd125bcae133b30be7a74f48da1e67d5
Administrator <100>
2022-11-13 19:08:29 +0800

    集群配置

---
+++ cloud-provider-payment8002/src/main/java/com/atguigu/springcloud/PaymentMain8002.java
@@ -0,0 +1,20 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author wangshentao
+ * @version 1.0
+ * @data 2022/10/24 11:28
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class PaymentMain8002 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8002.class, args);
+    }
+
+}
